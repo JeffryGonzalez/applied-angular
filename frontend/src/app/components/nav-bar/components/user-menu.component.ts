00fa6308736f374a85491d33393bba8dd8a041af
Jeff Gonzalez <jeff@hypertheory.com>
2024-07-15 12:33:41 -0400

    before lunch

---
+++ frontend/src/app/components/nav-bar/components/user-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-menu',
+  standalone: true,
+  imports: [],
+  template: `
+
+    <a class="btn">Click Me For Tacos</a>
+
+  `,
+  styles: ``
+})
+export class UserMenuComponent {
+
+}
