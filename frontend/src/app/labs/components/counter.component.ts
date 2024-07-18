c2bb612419d426dae632c8308f05f987eb3d5805
Daynya Quigley <daynya@gmail.com>
2024-07-18 13:55:39 -0400

    fizz buzz work

--- frontend/src/app/labs/components/counter.component.ts
+++ frontend/src/app/labs/components/counter.component.ts
@@ -14,24 +14,32 @@ import { Component, effect, signal } from '@angular/core';
         -
       </button>
       <button (click)="increment()" class="btn btn-primary">+</button>
+      @if (fizz) {
+        <p>fizz!</p>
+      }
+      @if (buzz) {
+        <p>buzz!!</p>
+      }
+      @if (fizzBuzz) {
+        <p>fizz buzz!!!</p>
+      }
     </div>`,
   styles: ``,
 })
 export class CounterComponents {
   disabled = false;
-  // constructor() {
-  //   const negativeCount = effect(() => {
-  //     if (this.num() <= 0) {
-  //       console.log('No more negativity!', this.num());
-  //     }
-  //   });
-  // }
+  fizz = false;
+  buzz = false;
+  fizzBuzz = false;
+
   num = signal(0);
+
   increment() {
     this.num.set(this.num() + 1);
     this.checkForDisabled();
     this.doFizzbuzzMath();
   }
+
   decrement() {
     this.num.set(this.num() - 1);
     this.checkForDisabled();
@@ -48,10 +56,21 @@ export class CounterComponents {
 
   doFizzbuzzMath() {
     if (this.num() % 3 === 0) {
-      console.log('fizz');
+      this.fizz = true;
+    } else {
+      this.fizz = false;
     }
     if (this.num() % 5 === 0) {
-      console.log('buzz');
+      this.buzz = true;
+    } else {
+      this.buzz = false;
+    }
+    if (this.num() % 3 === 0 && this.num() % 5 === 0) {
+      this.fizzBuzz = true;
+      this.fizz = false;
+      this.buzz = false;
+    } else {
+      this.fizzBuzz = false;
     }
   }
 }
