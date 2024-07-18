7f562b347bb252a23f825297f9411509dfe702a9
Jose R Hernandez <jose@markojudas.com>
2024-07-18 12:01:57 -0400

    add counter component

---
+++ frontend/src/app/labs/counter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  template: ` <p>THIS IS THE COUNTER COMPONENT!</p> `,
+  styles: ``,
+})
+export class CounterComponent {}
--- frontend/src/app/labs/labs.component.ts
+++ frontend/src/app/labs/labs.component.ts
@@ -1,16 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { JsonPipe } from '@angular/common';
 import { map } from 'rxjs';
+import { LinkDirective } from '../directives/link.directive';
+import { CounterComponent } from './counter.component';
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [RouterOutlet, JsonPipe],
+  imports: [
+    RouterOutlet,
+    JsonPipe,
+    RouterLink,
+    LinkDirective,
+    CounterComponent,
+  ],
   template: `
     <h2>Labs for Applied Angular</h2>
-
+    <ul>
+      <li><a appLink routerLink="counter">Counter</a></li>
+    </ul>
     <router-outlet></router-outlet>
   `,
   styles: ``,
