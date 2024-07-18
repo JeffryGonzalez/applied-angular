11c3447c3dd5c71b09747a16448d222f8543bd35
Jose R Hernandez <jose@markojudas.com>
2024-07-18 12:09:12 -0400

    Sprint 1 complete

--- frontend/src/app/labs/counter.component.ts
+++ frontend/src/app/labs/counter.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
   imports: [],
-  template: ` <p>THIS IS THE COUNTER COMPONENT!</p> `,
+  template: `
+    <p>THIS IS THE COUNTER COMPONENT!</p>
+    <p>Counter {{ counter() }}</p>
+    <div>
+      <button (click)="decrement()" class="btn btn-primary">-</button>
+      <button (click)="increment()" class="btn btn-primary">+</button>
+    </div>
+  `,
   styles: ``,
 })
-export class CounterComponent {}
+export class CounterComponent {
+  counter = signal(0);
+
+  decrement() {
+    this.counter.set(this.counter() - 1);
+  }
+  increment() {
+    this.counter.set(this.counter() + 1);
+  }
+}
